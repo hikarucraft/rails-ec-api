282b1c6a4034d97e82f3fb83ee01d4b9075255d3
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-12-18 15:57:01 +0900

    feat: configured sidekiq

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,13 @@
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: ENV['REDIS_URL'],
+    namespace: 'sidekiq',
+  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: ENV['REDIS_URL'],
+    namespace: 'sidekiq',
+  }
+end
